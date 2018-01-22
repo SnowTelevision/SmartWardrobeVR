137b199860c86fe6f5dbc725aaa3aeca7a1f5524
Peng Guan <pguan100@gmail.com>
2018-01-22 10:23:54 -0500

    Initial push, finish basic mirror functionality for SteamVR environment.

---
+++ Assets/Scripts/StayAtLocalPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayAtLocalPosition : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localEulerAngles = Vector3.zero;
+    }
+}
